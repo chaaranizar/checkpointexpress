804d6f0f37f02cf598522a3c3fc4f790275a1e7c
chaaranizar <chaaranizar@gmail.com>
2023-01-05 07:23:20 +0100

    first commit

--- verify.js
+++ verify.js
@@ -4,7 +4,7 @@ let hours = new Date().getHours();
 console.log (hours);
 
 function verify (req, res, next) {
-    if (day >0&& day<6 && hours > 8 && hours <19) next();
+    if (day >0&& day<6 && hours > 8 && hours <17) next();
     else
    res.send (`<h1>Welcome Our service is closed now, we open from Monday to Friday (8h to 17h)</h1>`);
 }
